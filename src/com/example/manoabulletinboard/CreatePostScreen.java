249e8e52f4c7bc07da0e9334267d704b1ebf57fb
DerrickTrang <trangd@hawaii.edu>
2014-12-01 18:02:16 -1000

    Added functional map with location settings

--- src/com/example/manoabulletinboard/CreatePostScreen.java
+++ src/com/example/manoabulletinboard/CreatePostScreen.java
@@ -13,18 +13,20 @@ import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class CreatePostScreen extends ActionBarActivity implements OnMapClickListener {
+public class CreatePostScreen extends ActionBarActivity implements OnMapClickListener, OnCameraChangeListener {
 	/*Attribute declaration*/
 	int test;
+	boolean markerPresent;
+	Marker postLocation;
 	TextView Title;
 	TextView Description;
 	TextView Email;
@@ -51,6 +53,8 @@ public class CreatePostScreen extends ActionBarActivity implements OnMapClickLis
 		// set default values for location
 		Location_x = 0;
 		Location_y = 0;
+		markerPresent = false;
+		postLocation = null;
 		
 		/*Assign view variables to xml objects*/
 		Title = (TextView)findViewById(R.id.create_post_screen_title);
@@ -68,6 +72,8 @@ public class CreatePostScreen extends ActionBarActivity implements OnMapClickLis
 			map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(21.29981552257571,-157.8175474)));
 			map.moveCamera(CameraUpdateFactory.zoomTo(16));
 			map.setOnMapClickListener(this);
+			map.setOnCameraChangeListener(this);
+			map.setMyLocationEnabled(true);
 //			Location tempLocation = map.getMyLocation();
 //			map.addMarker(new MarkerOptions().position(new LatLng(tempLocation.getLatitude(),tempLocation.getLongitude())).icon(
 //					BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
@@ -83,21 +89,27 @@ public class CreatePostScreen extends ActionBarActivity implements OnMapClickLis
 
 			@Override
 			public void onClick(View v) {
+				int length = Number.getText().toString().length();
+				Log.d("ManoaBulletinBoard","length = " + length);
 				if(Title.getText().toString().matches("") || Description.getText().toString().matches("") || Email.getText().toString().matches("") || Location_x == 0) {
                 	Toast toast = Toast.makeText(getApplicationContext(), "Missing some fields/location", Toast.LENGTH_SHORT);
+                	toast.show();
+				}
+				else if(length != 0 && length != 7 && length != 10) {
+                	Toast toast = Toast.makeText(getApplicationContext(), "Invalid phone number", Toast.LENGTH_SHORT);
+                	toast.show();
+                }
+				else if(!Email.getText().toString().contains("@")) {
+                	Toast toast = Toast.makeText(getApplicationContext(), "Invalid email", Toast.LENGTH_SHORT);
                 	toast.show();
 				}
 				else {
 				Log.d("ManoaBulletinBoard","Title is (" + Title.getText().toString() + ")");
 				Post UserPost = storeViewToObject();
-				// Post to server
-				// For now, send it back to the main activity for development
-				
 				
 				/*insert into the database*/
 				((PostApp)getApplication()).postdata.insert(UserPost);
 				((PostApp)getApplication()).AddEvent(UserPost);
-				
 				
 				Intent returnIntent = new Intent();
 				returnIntent.putExtra("result", "created");
@@ -129,16 +141,29 @@ public class CreatePostScreen extends ActionBarActivity implements OnMapClickLis
 	@Override
 	public void onMapClick(LatLng position) {
 		Log.d("ManoaBulletinBoard","Map was tapped");
-		map.addMarker(new MarkerOptions().position(position).title("Post here!"));
-		Location_x = position.longitude;
-		Location_y = position.latitude;
+		if(!markerPresent) {
+			markerPresent = true;
+			postLocation = map.addMarker(new MarkerOptions().position(position).title("Post here!"));
+			Location_x = position.longitude;
+			Location_y = position.latitude;			
+		}
+		else {
+			postLocation.remove();
+			postLocation = map.addMarker(new MarkerOptions().position(position).title("Post here!"));
+			Location_x = position.longitude;
+			Location_y = position.latitude;
+		}
 	}
 	
 	public Post storeViewToObject(){
-		
-		
+		int num;
+		if(Number.getText().toString().matches(""))
+			num = 0;
+		else
+			num = Integer.parseInt(Number.getText().toString());
+			
 		Post UserPost = new Post(Title.getContext(), 1, Title.getText().toString(),
-				"2014-11-15","02:55:00","03:55:00",Location_x,Location_y,"Post 208",Description.getText().toString(),Email.getText().toString(),Integer.parseInt(Number.getText().toString()),Category.getSelectedItem().toString());
+				"2014-11-15","02:55:00","03:55:00",Location_x,Location_y,"Post 208",Description.getText().toString(),Email.getText().toString(),num,Category.getSelectedItem().toString());
 		
 //		Post UserPost = new Post(Title.getText().toString(),
 //				Description.getText().toString(),Email.getText().toString(),
@@ -151,4 +176,25 @@ public class CreatePostScreen extends ActionBarActivity implements OnMapClickLis
 	
 	}
 	
+	@Override
+	public void onCameraChange(CameraPosition C) {
+		if(C.zoom < 16) {
+			map.moveCamera(CameraUpdateFactory.zoomTo(16));
+		}
+		if(C.zoom > 18) {
+			map.moveCamera(CameraUpdateFactory.zoomTo(18));
+		}
+		if(C.target.latitude < 21.29058395) {
+			map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(21.29058395,C.target.longitude)));
+		}
+		if(C.target.latitude > 21.3059279) {
+			map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(21.3059279,C.target.longitude)));
+		}
+		if(C.target.longitude > -157.81058) {
+			map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(C.target.latitude,-157.81058)));
+		}
+		if(C.target.longitude < -157.82444) {
+			map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(C.target.latitude,-157.82444)));
+		}
+	}
 }
--- src/com/example/manoabulletinboard/MainScreen.java
+++ src/com/example/manoabulletinboard/MainScreen.java
@@ -47,17 +47,16 @@ public class MainScreen extends ActionBarActivity {
         //pasing context into the application for showing dialog. (turns out only the serachbutton context works)
 		((PostApp)getApplication()).setContext(SearchButton.getContext());
         
-        //for testing Server only *******************
-        ServerB = (Button)findViewById(R.id.button1);
-        ServerB.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
-				((PostApp)getApplication()).SyncEvent();
-			}
-		});
+        //for testing Server only ******************* ADDED IMPLEMENTATION TO REFRESHLIST METHOD
+//        ServerB = (Button)findViewById(R.id.button1);
+//        ServerB.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				((PostApp)getApplication()).SyncEvent();
+//			}
+//		});
         //********************************************
         
         Refresh.setEnabled(true);
@@ -84,15 +83,6 @@ public class MainScreen extends ActionBarActivity {
 		    @Override 
 		    public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
 		    { 	
-//		    	Cursor c = (Cursor) list.getItemAtPosition(position);
-//		        Intent intent = new Intent(MainScreen.this, ViewPostScreen.class);
-//		        intent.putExtra("Title", c.getString(c.getColumnIndex("post_title")));
-//		        Log.d("ManoaBulletinBoard","Added title to intent");
-//		        intent.putExtra("Email", c.getString(c.getColumnIndex("post_email")));
-//		        Log.d("ManoaBulletinBoard","Added email to intent");
-//		        intent.putExtra("Description", c.getString(c.getColumnIndex("post_description")));
-//		        Log.d("ManoaBulletinBoard","Added description to intent");
-//		        startActivity(intent);
 		    	Post p = (Post) list.getItemAtPosition(position);
 		        Intent intent = new Intent(MainScreen.this, ViewPostScreen.class);
 		        intent.putExtra("Title", p.getName());
@@ -108,8 +98,8 @@ public class MainScreen extends ActionBarActivity {
 		    }
 		});
 		
-		// Reresh the listview
-		refreshList();
+		cursor = ((PostApp)getApplication()).postdata.query();
+		// Add something to make it refresh the list on create
     }
     
     @Override
@@ -155,7 +145,7 @@ public class MainScreen extends ActionBarActivity {
         if (requestCode == 1) {
             if(resultCode == RESULT_OK){
             	if(data.getStringExtra("result").equals("created") == true) {
-            		refreshList();
+            		// refreshList(); broken atm, add something that refreshes whole list after creating event
             		Toast toast = Toast.makeText(getApplicationContext(), "Event created", Toast.LENGTH_SHORT);
                 	toast.show();
                 }
@@ -166,11 +156,22 @@ public class MainScreen extends ActionBarActivity {
             }
             if (resultCode == RESULT_CANCELED) {
                 //Write your code if there's no result
+            	Toast toast = Toast.makeText(getApplicationContext(), "Event not created", Toast.LENGTH_SHORT);
+            	toast.show();
             }
         }
     }
     
-    public void refreshList() {		
+    public void refreshList() {
+    	Log.d("ManoaBulletinBoard","Refreshing...");
+    	try {
+    	// Sync internal database
+		((PostApp)getApplication()).SyncEvent();
+    	}
+    	catch (NullPointerException e) {
+        	Toast toast = Toast.makeText(getApplicationContext(), "Can't refresh", Toast.LENGTH_SHORT);
+        	toast.show();
+        }
     	// Refresh screen
 		cursor = ((PostApp)getApplication()).postdata.query();
 
@@ -193,6 +194,7 @@ public class MainScreen extends ActionBarActivity {
 			cursor.moveToNext();
 		}
     	adapter.notifyDataSetChanged();
+    	Log.d("ManoaBulletinBoard","Done refreshing");
     }
     
 }
